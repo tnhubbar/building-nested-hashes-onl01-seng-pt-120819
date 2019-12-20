f10be51134a5673fc6d5ab159c94ce6af637d374
Taryn Johnson <taryn.johnson919@gmail.com>
2019-12-20 01:27:38 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -4,16 +4,14 @@ def fourth_challenge
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
-      :hero_friends => [
-        #your hashes here!
+      :hero_friends => ["Benvolio", "17", "worried"
       ]
    }, 
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
-      :heroine_friends => [
-        #your hashes here!
+      :heroine_friends => ["Mercutio", "18", "hot-headed"
       ]
    }
 }
